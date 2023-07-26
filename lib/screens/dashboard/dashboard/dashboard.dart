7bdf71e8b3248bc9f16cdada99fcad7315b80893
Joyal <joyal670@gmail.com>
2023-07-26 12:46:50 +0530

    nav drawer

--- lib/screens/dashboard/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard/dashboard.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 import 'package:ncomfort/screens/common/widgets.dart';
@@ -29,7 +29,7 @@ class _DashboardScreenState extends State<DashboardScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
+        automaticallyImplyLeading: true,
         backgroundColor: colorCasal,
         bottom: TabBar(
             controller: _tabController,
@@ -89,10 +89,204 @@ class _DashboardScreenState extends State<DashboardScreen>
               )
             ]),
       ),
+      drawer: NavDrawer(),
       body: TabBarView(
         controller: _tabController,
         children: [MyPropertyScreen(), EnquiryScreen()],
       ),
     );
+  }
+}
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: Column(
+      children: [
+        DrawerHeader(
+          child: Center(
+            child: Column(
+              children: [
+                height10,
+                CircleAvatar(
+                  radius: 35,
+                  backgroundColor: colorCasal,
+                ),
+                height10,
+                TextWidget(
+                  title: 'Daniel A. Castiglia',
+                  textStyle: TextStyle(
+                      color: colorTundora,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14),
+                ),
+                height5,
+                TextWidget(
+                  title: 'ThomasMBain@dayre.com',
+                  textStyle: TextStyle(
+                      color: colorTundora,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 13),
+                )
+              ],
+            ),
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(color: colorCasal),
+            child: Padding(
+              padding: const EdgeInsets.all(25),
+              child: ListView(
+                children: [
+                  ListTile(
+                    dense: true,
+                    visualDensity: VisualDensity(vertical: -2),
+                    horizontalTitleGap: 0,
+                    leading: Image.asset(
+                      'assets/images/language.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                    title: TextWidget(
+                      title: 'عربى',
+                      textStyle: TextStyle(
+                          color: colorWhite,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13),
+                    ),
+                    onTap: () => {},
+                  ),
+                  ListTile(
+                    dense: true,
+                    visualDensity: VisualDensity(vertical: -2),
+                    horizontalTitleGap: 0,
+                    leading: Image.asset(
+                      'assets/images/user.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                    title: TextWidget(
+                      title: 'My Profile',
+                      textStyle: TextStyle(
+                          color: colorWhite,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13),
+                    ),
+                    onTap: () => {Navigator.of(context).pop()},
+                  ),
+                  ListTile(
+                    dense: true,
+                    visualDensity: VisualDensity(vertical: -2),
+                    horizontalTitleGap: 0,
+                    leading: Image.asset(
+                      'assets/images/bar-chart-2_white.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                    title: TextWidget(
+                      title: 'My Properties',
+                      textStyle: TextStyle(
+                          color: colorWhite,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13),
+                    ),
+                    onTap: () => {Navigator.of(context).pop()},
+                  ),
+                  ListTile(
+                    dense: true,
+                    visualDensity: VisualDensity(vertical: -2),
+                    horizontalTitleGap: 0,
+                    leading: Image.asset(
+                      'assets/images/dollar-sign.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                    title: TextWidget(
+                      title: 'Bank Details',
+                      textStyle: TextStyle(
+                          color: colorWhite,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13),
+                    ),
+                    onTap: () => {Navigator.of(context).pop()},
+                  ),
+                  height30,
+                  ListTile(
+                    dense: true,
+                    visualDensity: VisualDensity(vertical: -4),
+                    horizontalTitleGap: 0,
+                    title: TextWidget(
+                      title: 'Legal Information',
+                      textStyle: TextStyle(
+                          color: colorWhite,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13),
+                    ),
+                    onTap: () => {},
+                  ),
+                  ListTile(
+                    dense: true,
+                    visualDensity: VisualDensity(vertical: -4),
+                    horizontalTitleGap: 0,
+                    title: TextWidget(
+                      title: 'Privacy Policy',
+                      textStyle: TextStyle(
+                          color: colorWhite,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13),
+                    ),
+                    onTap: () => {},
+                  ),
+                  ListTile(
+                    dense: true,
+                    visualDensity: VisualDensity(vertical: -4),
+                    horizontalTitleGap: 0,
+                    title: TextWidget(
+                      title: 'FAQ',
+                      textStyle: TextStyle(
+                          color: colorWhite,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13),
+                    ),
+                    onTap: () => {},
+                  ),
+                  ListTile(
+                    dense: true,
+                    visualDensity: VisualDensity(vertical: -4),
+                    horizontalTitleGap: 0,
+                    title: TextWidget(
+                      title: 'Feedback',
+                      textStyle: TextStyle(
+                          color: colorWhite,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13),
+                    ),
+                    onTap: () => {},
+                  ),
+                  ListTile(
+                    dense: true,
+                    visualDensity: VisualDensity(vertical: -4),
+                    horizontalTitleGap: 0,
+                    title: TextWidget(
+                      title: 'Logout',
+                      textStyle: TextStyle(
+                          color: colorWhite,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13),
+                    ),
+                    onTap: () => {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
